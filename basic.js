847fe37f0179bdc33e83807ca0cfc6b83b20378d
Manish V <imvmanish19@gmail.com>
2020-06-16 13:15:55 +0530

    Op

--- basic.js
+++ basic.js
@@ -1,11 +1,53 @@
 let dotenv = require('dotenv').config();
 let Sequelize = require('sequelize');
-
+let DataTypes = Sequelize.DataTypes;
+const Op = Sequelize.Op
+//creating an instance of the database
 let db = new Sequelize('testdb2','alien',process.env.pass,{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    operatorAliases: {
+        $gt: Op.gt
+    }
 });
 
+
+//test the connection
 db.authenticate()
 .then(() => console.log("Connected"))
 .catch((error) => console.error(error));
+
+//creating a model
+
+let Coders = db.define('codingAliens',{
+    name: DataTypes.STRING(20),
+    username: DataTypes.STRING(12),
+    technology: DataTypes.STRING(20),
+    age: {
+        type: DataTypes.INTEGER(2),
+        allowNull: false
+    }
+});
+
+//Using the model to create table
+db.sync()
+.then(() => console.log("Sync Done"))
+.catch(console.error)
+
+//Inserting
+
+// Coders.create({
+//     name: 'XXABCDEF',
+//     username: 'XXXXX',
+//     technology: 'JSSS',
+//     age: 21
+// })
+
+//Querying
+
+Coders.findAll({
+    where: {age: {[Op.gt]: 14}}
+})
+.then((coders) => {
+    coders.forEach(coder => console.log(coder.dataValues) )
+});
